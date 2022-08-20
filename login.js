14cde91c4fe8050bf6655a10d59fd92a6253b450
Haroldy <HaroldM09@outlook.com>
2022-08-20 13:56:20 -0400

    Inicio de sesion completado

--- login.js
+++ login.js
@@ -9,17 +9,16 @@ boton.addEventListener("click", (e) => {
     const email = correo.value;
     const pass = clave.value;
 
+    vacioLogin(email, pass);
+
+});
+
+function vacioLogin(email, pass) {
+
     if(email !== "" & pass !== "") {
         
-        swal.fire({
-        icon: 'success',
-        text:'El usuario inicio sesión con exito.',
-        showConfirmButton: false,
-
-        });
-
-        window.location.href = "/index.html";
-
+        valEmail(email)
+        
     } else {
 
         swal.fire({
@@ -35,8 +34,43 @@ boton.addEventListener("click", (e) => {
             }
         });
     }
-});
+}
 
+function valEmail(valor){  
+    
+    re=/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/
+    if(!re.exec(valor)) {
+
+        swal.fire({
+            icon: 'info',
+            text:'EL correo no es valido',
+            showConfirmButton: false,
+            showClass: {
+                popup: 'animate__animated animate__shakeX'
+            },
+    
+            hideClass:{
+                popup: 'animate__animated animate__fadeOut'
+                }
+            });
+
+        return false;
+
+    } else {
+
+        swal.fire({
+            icon: 'success',
+            text:'El usuario inicio sesión con exito.',
+            showConfirmButton: false,
+    
+            });
+    
+            window.location.href = "/index.html";
+
+        return true;
+    }
+
+}
 
 
 
